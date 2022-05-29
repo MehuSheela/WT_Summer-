3284de8d4532d76a020f89de796fb0cc4987580d
MehuSheela <103553812+MehuSheela@users.noreply.github.com>
2022-05-30 00:36:16 +0600

    Update and rename Task1 to Process.php

--- Process.php
+++ Process.php
@@ -1,23 +1,23 @@
 <?php
 $number1=$_POST["number1"];
 $number2=$_POST["number2"];
-$Operation=$_POST["Operation"];
+$Series=$_POST["Series"];
 $number1=(float)$number1;
 $number2=(float)$number2;
 
-if ($Operation=="add")
+if ($Series=="add")
 {
     echo "Your 1st number is ".$number1." and your 2nd number is ".$number2." and summation is ".$number1+$number2;
 }
-if ($Operation=="substraction")
+if ($Series=="substraction")
 {
     echo "Your 1st number is ".$number1." and your 2nd number is ".$number2." and substraction is ".$number1-$number2;
 }
-if ($Operation=="multiplication")
+if ($Series=="multiplication")
 {
     echo "Your 1st number is ".$number1." and your 2nd number is ".$number2." and multiplication is ".$number1*$number2;
 }
-if ($Operation=="division")
+if ($Series=="division")
 {
     echo "your 1st number is ".$number1." and your 2nd number is ".$number2." and division is ".$number1/$number2;
 }
